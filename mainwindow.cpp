f34ba882ee0b329138f2dbeaac1c77271b3ac577
Andrey Shmelev <shmelev@korvas.org>
2018-03-20 11:24:54 +0500

    камера захватывается

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,83 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QCameraImageCapture>
+#include <QList>
+#include <QDebug>
+#include <QCameraInfo>
+#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QLayoutItem>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //    mCamera = new QCamera(this);
+    mCameraViewfinder = new QCameraViewfinder(this);
+    //    mCameraImageCapture = new QCameraImageCapture(this);
+    mLayout = new QVBoxLayout(this);
+
+    qDebug() << "availableCameras" << QCameraInfo::availableCameras();
+
+    QList<QCameraInfo> camerainfos = QCameraInfo::availableCameras();
+
+    if (camerainfos.isEmpty())
+    {
+        ShowMessageBox("Камеры не найдены");
+    }
+    else
+    {
+        mCamera = new QCamera(camerainfos.at(0));
+        mCameraViewfinder = new QCameraViewfinder(this);
+        mCameraImageCapture = new QCameraImageCapture(mCamera,this);
+        mCamera->setViewfinder(mCameraViewfinder);
+        mLayout->addWidget(mCameraViewfinder);
+        mLayout->setMargin(0);
+        ui->scrollArea->setLayout(mLayout);
+    }
+
+    foreach (const QCameraInfo &cameraInfo, camerainfos) {
+        //        mCamera = new QCamera(cameraInfo);
+        //        cameras.append(mCamera);
+        //        ui->CameraList->addItem(cameraInfo.description() + cameraInfo.deviceName());
+    }
+
+    mCameraImageCapture->setCaptureDestination(QCameraImageCapture::CaptureToBuffer);
+
+    QImageEncoderSettings encSettings;
+    encSettings.setCodec("image/jpeg");
+    encSettings.setResolution(800,600);
+
+    mCameraImageCapture->setEncodingSettings(encSettings);
+    mCamera->setCaptureMode(QCamera::CaptureVideo);
+
+    mCamera->start();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::ShowMessageBox(QString message)
+{
+    QMessageBox msgBox;
+    msgBox.setText(message);
+    msgBox.setInformativeText(message);
+    msgBox.setStandardButtons(QMessageBox::Ok);
+    msgBox.setDefaultButton(QMessageBox::Ok);
+    msgBox.setStyleSheet("QMessageBox{background-color: rgb(103,158,210);color: white;}QPushButton \
+    {\
+                             background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #E25303, stop: 0.1 #E25303, stop: 0.49 #E25303, stop: 0.5 #E25303, stop: 1 #E25306);\
+                             border-style: outset;\
+                             border-width: 2px;\
+                             border-radius: 10px;\
+                             border-color: beige;\
+                             font: bold 14px; color: white;\
+                             padding: 6px;\
+                         }\
+                         ");
+                         int ret = msgBox.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCamera>
+#include <QCameraViewfinder>
+#include <QCameraImageCapture>
+#include <QVBoxLayout>
+#include <QLayoutItem>
 
 namespace Ui {
 class MainWindow;
 }
+
 
 class MainWindow : public QMainWindow
 {
@@ -15,8 +21,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void ShowMessageBox(QString message);
 private:
     Ui::MainWindow *ui;
+
+
+    QList<QCamera * > cameras;
+
+
+    QCamera *mCamera;
+    QCameraViewfinder *mCameraViewfinder;
+    QCameraImageCapture *mCameraImageCapture;
+    QVBoxLayout *mLayout;
 };
 
 #endif // MAINWINDOW_H
